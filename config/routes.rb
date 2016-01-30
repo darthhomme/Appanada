d479815d654817a348bfd0f917c7b3fd55e15d72
Jeong Kim <joshkim83@outlook.com>
2016-01-30 14:44:35 -0500

    adding controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  post '/sessions' => 'sessions#create'
+  delete '/sessions' => 'sessions#destroy'
 
+  resources :employees
+  get '/log_in' => 'employees#log_in'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
