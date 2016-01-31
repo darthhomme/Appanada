aa5afd57f83717d7373f0a97f89999c8518adaee
Jeong Kim <joshkim83@outlook.com>
2016-01-30 21:07:36 -0500

    styling

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -17,3 +17,12 @@ class EmployeesController < ApplicationController
     params.require(:employee).permit(:firstname, :lastname, :username, :password_digest)
   end
 end
+
+# employees GET    /employees(.:format)          employees#index
+#           POST   /employees(.:format)          employees#create
+# new_employee GET    /employees/new(.:format)      employees#new
+# edit_employee GET    /employees/:id/edit(.:format) employees#edit
+#  employee GET    /employees/:id(.:format)      employees#show
+#           PATCH  /employees/:id(.:format)      employees#update
+#           PUT    /employees/:id(.:format)      employees#update
+#           DELETE /employees/:id(.:format)      employees#destroy
